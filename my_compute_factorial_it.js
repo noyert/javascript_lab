f265b0295c59d75b47e934c4e0478f3a490301a1
Théophile Noyer <theophile.noyer@hotmail.fr>
2019-11-12 11:11:18 +0100

    Task 01

--- my_compute_factorial_it.js
+++ my_compute_factorial_it.js
@@ -1,4 +1,20 @@
 
-function ​my_compute_factorial_it (nb) {
-    return nb;
+function my_compute_factorial_it(nb) {
+    
+    var factorial = 1;
+    if(Number.isInteger(nb)){
+        for (var i = 2; i <= nb; i++) {
+            factorial *= i;
+        }
+        console.log(`Le résultat est ${factorial}`)
+        return factorial;
+    } else {
+        console.log("Votre donnée n'est pas un nombre")
+        return 0;
+    }
 }
+
+var argv = require('minimist')(process.argv.slice(2));
+console.dir(argv._[0]);
+
+my_compute_factorial_it(argv._[0]);
