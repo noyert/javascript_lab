9ec672ae0d557b8df13d169aab3a60ad83512437
Théophile Noyer <theophile.noyer@hotmail.fr>
2019-11-13 11:58:08 +0100

    task2

--- day01/index.js
+++ day01/index.js
@@ -1,18 +1,29 @@
-var func = require('./my_to_upper_case')
+var func1 = require('./my_to_upper_case')
+var func2 = require('./my_to_lower_case')
 
 var argv = require('minimist')(process.argv.slice(2));
 // console.dir(argv._[0]);
 
-var res = argv._[0];
+var res1 = argv._[0];
+var res2 = argv._[1];
 
-String.prototype.my_to_upper_case = func;
+String.prototype.my_to_upper_case = func1;
+String.prototype.my_to_lower_case = func2;
 
-var result = res.my_to_upper_case()
+var result1 = res1.my_to_upper_case()
+var result2 = res2.my_to_lower_case()
 
 // console.log("hello".my_to_upper_case())
 
-if(typeof res === 'string'){
-    console.log(`Résultat: ${result}`)
+if(typeof res1 === 'string'){
+    console.log(`Résultat: ${result1}`)
+} else {
+    console.log("Votre donnée n'est pas de type string")
+    return;
+}
+
+if(typeof res2 === 'string'){
+    console.log(`Résultat: ${result2}`)
 } else {
     console.log("Votre donnée n'est pas de type string")
     return;
---
+++ day01/my_to_lower_case.js
@@ -0,0 +1,15 @@
+module.exports = function my_to_lower_case(){
+    let inputStr = this
+    var strResult = ""
+    for (var i = 0; i < inputStr.length; i++) {
+
+        var letters = inputStr.charCodeAt(i)
+
+        if (letters > 64 && letters < 91) {
+            strResult += String.fromCharCode(letters + 32)
+        } else {
+            strResult += inputStr.charAt(i)
+        }
+    }
+    return strResult
+ }
