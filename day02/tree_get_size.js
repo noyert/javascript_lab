39c09a1ad6533a7daf1f6130107566650122811b
Théophile Noyer <theophile.noyer@hotmail.fr>
2019-11-20 10:18:21 +0100

    tree get size

--- day02/tree_get_size.js
+++ day02/tree_get_size.js
@@ -3,12 +3,23 @@ module.exports = function tree_get_size(tree) {
         tree.data = 0
         return
     }
-    var nb = 0
-    let current = tree
-    while (current !== null) {
-        current = current.left;
-        nb++
+    function hello(node) {
+
+        if (node.left == null && node.right == null){
+            return 1
+        }
+    
+        if (node.left){
+            const sizeLeft = hello(node.left)
+        }
+    
+        if (node.right){
+            const sizeRight = hello(node.right)
+        }
+    
+        return 1 + sizeLeft + sizeRight
     }
+<<<<<<< Updated upstream
     current = tree
     while(current !== null){
         current = current.right;
@@ -16,3 +27,19 @@ module.exports = function tree_get_size(tree) {
     }
     return nb-1
 }
+=======
+    var nb = hello(tree)
+    // var nb = 0
+    // let current = tree
+    // while (current !== null) {
+    //     current = current.left;
+    //     nb++
+    // }
+    // current = tree
+    // while(current !== null){
+    //     current = current.right;
+    //     nb++
+    // }
+    return nb
+}
+>>>>>>> Stashed changes
---
+++ day03/index.js
@@ -0,0 +1,5 @@
+const square = require('./square')
+const tree = require('./tree')
+
+// console.log(square(5, 6))
+// console.log(tree(5))
