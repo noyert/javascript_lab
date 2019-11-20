37afad963369d6dc9b194fedd65db239a529bad6
Théophile Noyer <theophile.noyer@hotmail.fr>
2019-11-20 12:01:10 +0100

    function priority calc

--- bistro/bistro.js
+++ bistro/bistro.js
@@ -7,48 +7,20 @@ function bistro(str) {
         return false;
     }
 
-    function isSeparator(separator) {
-        if (separator == '+' || separator == '-' || separator == '*' || separator == '/' || separator == '%' || separator == '(' || separator == ')') {
-            return true
-        }
-        return false
-    }
-
-    function splitNumber(str) {
-        var numbers = [""];
-        if (typeof str === 'string') {
-            for (var i = 0; i < str.length; i++) {
-                if (!isSeparator(str[i])) {
-                    numbers[numbers.length - 1] += str[i];
-                } else if (numbers[numbers.length - 1]) {
-                    numbers.push("");
-                }
-            }
-            return numbers
-        }
-    }
-
-    function splitOperator(str) {
-        var operators = [""];
-        if (typeof str === 'string') {
-            for (var i = 0; i < str.length; i++) {
-                if (isOperator(str[i])) {
-                    operators[operators.length - 1] += str[i];
-                } else if (operators[operators.length - 1]) {
-                    operators.push("");
-                }
-            }
-            return operators
-        }
-    }
-
     function split(str) {
         var split = [""];
         if (typeof str === 'string') {
             for (var i = 0; i < str.length; i++) {
-                if (!isSeparator(str[i])) {
+                if (!isOperator(str[i]) && str[i] == '(') {
                     split[split.length - 1] += str[i];
-                } else if (split[split.length - 1]) {
+                    split.push("");
+                } else if(!isOperator(str[i]) && str[i] == ')') {
+                    split.push("");
+                    split[split.length - 1] += str[i];
+                } else if(!isOperator(str[i])){
+                    split[split.length - 1] += str[i];
+                }
+                else if (split[split.length - 1]) {
                     split.push("");
                     split[split.length - 1] += str[i];
                     split.push("");
@@ -58,44 +30,21 @@ function bistro(str) {
         }
     }
 
-    // var numbers = splitNumber(str)
-    // console.log(numbers)
-    // var ops = splitOperator(str)
-    // console.log(ops)
     var elem = split(str)
     console.log(elem)
 
-    var f = {
-        add: '+',
-        sub: '-',
-        div: '/',
-        mlt: '*',
-        mod: '%',
-    };
-
-    // Create array for Order of Operation and precedence
-    f.ooo = [[[f.mlt], [f.div], [f.mod]], [[f.add], [f.sub]]];
-    // console.log(f.ooo[0][1])
-
-    str = str.replace(/[^0-9%^*\/()\-+.]/g, '');
-
-    var fLength = f.ooo.length
-    // console.log(fLength)
-
-    var tab = elem
-    console.log(tab)
-
     function priorityCalc(elem) {
         var calc = ""
-        // for(var i=0; i<elem.length; i++){
-        //     console.log(elem)
-        //     if(elem[i] == '('){
-        //         console.log("op (")
-        //         calc = calculate(elem[i+1], elem[i+2], elem[i+3])
-        //         elem.splice(elem[i+1], 5, calc)
-        //         i=0
-        //     }
-        // }
+        for(var i=0; i<elem.length; i++){
+            console.log(elem)
+            if(elem[i] == '('){
+                console.log("op ()")
+                console.log(elem[i+2])
+                calc = calculate(elem[i+1], elem[i+2], elem[i+3])
+                elem.splice(i, 5, calc)
+                i=0
+            }
+        }
         for (var i = 0; i < elem.length; i++) {
             console.log(elem)
             if (elem[i] == '*' || elem[i] == '/' || elem[i] == '%') {
@@ -106,7 +55,7 @@ function bistro(str) {
             }
         }
         for (var i = 0; i < elem.length; i++) {
-            console.log(elem)
+            // console.log(elem)
             if (elem[i] == '+' || elem[i] == '-') {
                 console.log("op2")
                 calc = calculate(elem[i - 1], elem[i], elem[i + 1])
@@ -117,28 +66,13 @@ function bistro(str) {
         return calc
     }
 
-    var result = priorityCalc(elem)
-    return result
-
-    // var output = 0
-    // for (var i = 0; i < fLength; i++) {
-
-    //     // Regular Expression to look for operators between floating numbers or integers
-    //     var re = new RegExp('(\\d+\\.?\\d*)([\\' + f.ooo[i].join('\\') + '])(\\d+\\.?\\d*)');
-    //     re.lastIndex = 0; // take precautions and reset re starting pos
-
-    //     // Loop while there is still calculation for level of precedence
-    //     while (re.test(str)) {
-    //         output = calculate(RegExp.$1, RegExp.$2, RegExp.$3);
-    //         if (isNaN(output) || !isFinite(output)){
-    //             console.log("output is not a number")
-    //             return output; // exit early if not a number
-    //         }
-    //         str = str.replace(re, output);
-    //     }
-    // }
-
-    // return output
+    var f = {
+        add: '+',
+        sub: '-',
+        div: '/',
+        mlt: '*',
+        mod: '%',
+    };
 
     function calculate(a, op, b) {
         if (isNaN(a) || isNaN(b)) {
@@ -174,7 +108,10 @@ function bistro(str) {
             return
         }
     }
+
+    var result = priorityCalc(elem)
+    return result
 }
 
-var res = bistro("10+2*3")
+var res = bistro("20*(3+2)*(4-2)")
 console.log(res)
