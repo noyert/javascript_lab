5bd0f350fb9c199b8ebb38957168f6f90330e838
Théophile Noyer <theophile.noyer@hotmail.fr>
2019-11-22 18:11:57 +0100

    function add + sub in progress

--- bistro/bistro.js
+++ bistro/bistro.js
@@ -34,7 +34,7 @@ function bistro(str) {
         var split = [""];
         if (typeof str === 'string') {
             for (var i = 0; i < str.length; i++) {
-                 if (str[i] !== ".") {
+                if (str[i] !== ".") {
                     split[split.length - 1] += str[i];
                 }
                 else if (split[split.length - 1]) {
@@ -46,17 +46,17 @@ function bistro(str) {
     }
 
     function spliceTab(tab, index, nbIndex, indexReplace) {
-        if(index+nbIndex > tab.length){
+        if (index + nbIndex > tab.length) {
             console.log("Votre nombre d'éléments à remplacer est supérieur à la taille du tableau")
-            return 
+            return
         }
         var newTab = []
         var counter = 0;
-        for(var i=0; i<tab.length; i++){
-            if(i == index) {
+        for (var i = 0; i < tab.length; i++) {
+            if (i == index) {
                 newTab[counter] = indexReplace
                 counter++
-            } else if(i < index || i > index + nbIndex -1){
+            } else if (i < index || i > index + nbIndex - 1) {
                 newTab[counter] = tab[i]
                 counter++
             }
@@ -76,12 +76,13 @@ function bistro(str) {
             return split
         }
     }
+
     function round(res) {
         let resString = `${res}`
         let resSplited = splitPoint(resString)
         let newSplit = simpleSplit(resSplited[1])
         let newRes = 0
-        if(newSplit == null || newSplit === undefined){
+        if (newSplit == null || newSplit === undefined) {
             return res
         }
         for (var i = 0; i < resSplited.length; i++) {
@@ -99,11 +100,11 @@ function bistro(str) {
 
     function priorityCalc(elem) {
         var calc = ""
-        while(elem.indexOf('(') !== -1){
+        while (elem.indexOf('(') !== -1) {
             var tab = []
             counter = 0
             for (var i = elem.indexOf('('); i < elem.indexOf(')') - 1; i++) {
-                tab.push(elem[i+1])
+                tab.push(elem[i + 1])
             }
             var length = tab.length + 2
             for (var i = 0; i < tab.length; i++) {
@@ -147,21 +148,101 @@ function bistro(str) {
         mod: '%',
     };
 
+    function add(str1, str2) {
+
+        let sum = "";
+
+        let str1Length = str1.length;
+        let str2Length = str2.length;
+
+        if (str2Length > str1Length) {
+            let temp = str2;
+            str2 = str1;
+            str1 = temp;
+        }
+
+        let carry = 0;  // number that is carried to next decimal place, initially zero.
+        let a;
+        let b;
+        let temp;
+        let digitSum;
+
+        for (let i = 0; i < str1.length; i++) {
+            // get ith digit of str1 from right, we store it in a
+            a = parseInt(str1.charAt(str1.length - 1 - i));
+            // get ith digit of str2 from right, we store it in b  
+            b = parseInt(str2.charAt(str2.length - 1 - i));
+            // make sure b is a number, (this is useful in case, str2 is shorter than str1   
+            b = (b) ? b : 0;
+            // add a and b along with carry, store it in a temp string.
+            temp = (carry + a + b).toString();
+            digitSum = temp.charAt(temp.length - 1);
+            // split the string into carry and digitSum ( least significant digit of abSum.
+            carry = parseInt(temp.substr(0, temp.length - 1));
+            // if carry is not number, make it zero.
+            carry = (carry) ? carry : 0;
+            // append digitSum to 'sum'. If we reach leftmost digit, append abSum which includes carry too.
+            sum = (i === str1.length - 1) ? temp + sum : digitSum + sum;
+        }
+        return sum;
+    }
+
+    function sub(str1, str2) {
+        let sum = "";
+
+        let str1Length = str1.length;
+        let str2Length = str2.length;
+
+        if (str2Length > str1Length) {
+            let temp = str2;
+            str2 = str1;
+            str1 = temp;
+        }
+
+        let carry = 0;  // number that is carried to next decimal place, initially zero.
+        let a;
+        let b;
+        let temp;
+        let digitSum;
+
+        for (let i = 0; i < str1.length; i++) {
+            // get ith digit of str1 from right, we store it in a
+            a = parseInt(str1.charAt(str1.length - 1 - i));
+            // get ith digit of str2 from right, we store it in b  
+            b = parseInt(str2.charAt(str2.length - 1 - i));
+            // make sure b is a number, (this is useful in case, str2 is shorter than str1   
+            b = (b) ? b : 0;
+            // add a and b along with carry, store it in a temp string.
+            temp = ((a-carry) - b).toString();
+            // console.log(temp)
+            digitSum = temp.charAt(temp.length - 1);
+            // console.log(digitSum)
+            // split the string into carry and digitSum ( least significant digit of abSum.
+            carry = parseInt(temp.substr(0, temp.length - 1));
+            console.log(carry)
+            // if carry is not number, make it zero.
+            carry = (carry) ? carry : 0;
+            // append digitSum to 'sum'. If we reach leftmost digit, append abSum which includes carry too.
+            sum = (i === str1.length - 1) ? temp + sum : digitSum + sum;
+            // console.log(i)
+            // console.log(str1.length - 1)
+        }
+        return sum;
+    }
+
     function calculate(a, op, b) {
         if (isNaN(a) || isNaN(b)) {
             console.log("Un des nombres n'est pas valide")
             return
         }
-        a = a * 1;
-        b = b * 1;
         let result = ''
         if (isOperator(op)) {
             switch (op) {
                 case f.add:
-                    result = a + b;
+                    result = add(a, b);
                     break;
                 case f.sub:
-                    result = a - b;
+                    result = sub(a, b);
                     break;
                 case f.div:
                     result = a / b;
@@ -182,13 +263,13 @@ function bistro(str) {
         }
     }
 
-    function mySubStr(str, index, length){
-        if(length == undefined || length == null){
+    function mySubStr(str, index, length) {
+        if (length == undefined || length == null) {
             length = str.length - index
         }
         var nexStr = ""
-        if (typeof str === 'string'){
-            for(var i=index; i <= index + length - 1; i++){
+        if (typeof str === 'string') {
+            for (var i = index; i <= index + length - 1; i++) {
                 nexStr += str[i]
             }
         } else {
@@ -198,35 +279,10 @@ function bistro(str) {
         return nexStr
     }
 
-    function bigNumber(nb){
-        if(nb === Infinity){
-            return console.log("le nombre entrer est considéré comme infini")
-        }
-        if(nb<=Number.MAX_SAFE_INTEGER){
-            return `${nb}`
-        }
-        var result = '';
-        var xStr = `${nb}`;
-        var digitCount = xStr.indexOf('e') === -1 ? xStr.length : ((mySubStr(xStr, xStr.indexOf('e') + 1))*1 + 1)
-        for (var i = 1; i <= digitCount; i++) {
-          var mod = `${(nb % 10**i)}`;
-          var exponent = (mod.indexOf('e') === -1) ? 0 : (mod.substr(mod.indexOf('e')+1))*1;
-          if ((exponent === 0 && mod.length !== i) || (exponent > 0 && exponent !== i-1)) {
-            result = '0' + result;
-          }
-          else {
-            result = mod.charAt(0) + result;
-          }
-        }
-        return result;
-    }
-
     var result = priorityCalc(elem)
-    result = bigNumber(result)
     result = round(result)
-    // result = bigNumber(result)
     return result
 }
 
-var res = bistro("(22+23+21)*2")
+var res = bistro("(23-8)")
 console.log(res)
