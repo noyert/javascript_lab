6e7a1b5b6c898771c3275e6e962584d3d1917d01
Théophile Noyer <theophile.noyer@hotmail.fr>
2019-11-23 14:48:14 +0100

    function sub

--- bistro/bistro.js
+++ bistro/bistro.js
@@ -77,6 +77,22 @@ function bistro(str) {
         }
     }
 
+    function mySubStr(str, index, length) {
+        if (length == undefined || length == null) {
+            length = str.length - index
+        }
+        var nexStr = ""
+        if (typeof str === 'string') {
+            for (var i = index; i <= index + length - 1; i++) {
+                nexStr += str[i]
+            }
+        } else {
+            console.log("Vos données ne sont pas de type string")
+            return;
+        }
+        return nexStr
+    }
+
     function round(res) {
         let resString = `${res}`
         let resSplited = splitPoint(resString)
@@ -169,16 +185,17 @@ function bistro(str) {
 
         for (let i = 0; i < str1.length; i++) {
             // get ith digit of str1 from right, we store it in a
-            a = parseInt(str1.charAt(str1.length - 1 - i));
+            a = str1.charAt(str1.length - 1 - i)*1;
             // get ith digit of str2 from right, we store it in b  
-            b = parseInt(str2.charAt(str2.length - 1 - i));
+            b = str2.charAt(str2.length - 1 - i)*1;
             // make sure b is a number, (this is useful in case, str2 is shorter than str1   
             b = (b) ? b : 0;
             // add a and b along with carry, store it in a temp string.
-            temp = (carry + a + b).toString();
+            temp = `${(carry + a + b)}`
+            // console.log(temp)
             digitSum = temp.charAt(temp.length - 1);
             // split the string into carry and digitSum ( least significant digit of abSum.
-            carry = parseInt(temp.substr(0, temp.length - 1));
+            carry = temp.substr(0, temp.length - 1)*1;
             // if carry is not number, make it zero.
             carry = (carry) ? carry : 0;
             // append digitSum to 'sum'. If we reach leftmost digit, append abSum which includes carry too.
@@ -189,6 +206,14 @@ function bistro(str) {
 
     function sub(str1, str2) {
         let sum = "";
+        let isNegative = false
+
+        if(str1 < str2){
+            let temp = str2;
+            str2 = str1;
+            str1 = temp;
+            isNegative = true
+        }
 
         let str1Length = str1.length;
         let str2Length = str2.length;
@@ -213,22 +238,31 @@ function bistro(str) {
             // make sure b is a number, (this is useful in case, str2 is shorter than str1   
             b = (b) ? b : 0;
             // add a and b along with carry, store it in a temp string.
-            temp = ((a-carry) - b).toString();
-            // console.log(temp)
-            digitSum = temp.charAt(temp.length - 1);
-            // console.log(digitSum)
+            temp = `${((a-carry) - b)}` 
+            digitSum = (temp < 0) ? `${(10+(temp)*1)}` : temp
             // split the string into carry and digitSum ( least significant digit of abSum.
-            carry = parseInt(temp.substr(0, temp.length - 1));
-            console.log(carry)
+            carry = (temp < 0) ? 1 : 0
             // if carry is not number, make it zero.
             carry = (carry) ? carry : 0;
             // append digitSum to 'sum'. If we reach leftmost digit, append abSum which includes carry too.
             sum = (i === str1.length - 1) ? temp + sum : digitSum + sum;
-            // console.log(i)
-            // console.log(str1.length - 1)
+        }
+        sum = (sum[0] == 0) ? mySubStr(sum, 1) : sum
+        if(isNegative){
+            sum = '-' + sum
         }
         return sum;
     }
+
+    function mod(a, b) 
+    { 
+        // Initialize result 
+        var res = 0; 
+        // One by one process all digits of 'num' 
+        for (var i = 0; i < a.length; i++) 
+            res = (res * 10 + (a.charAt(i))*1 - '0') % b; 
+        return res; 
+    } 
 
     function calculate(a, op, b) {
         if (isNaN(a) || isNaN(b)) {
@@ -251,7 +285,7 @@ function bistro(str) {
                     result = a * b;
                     break;
                 case f.mod:
-                    result = a % b;
+                    result = mod(a, b);
                     break;
                 default:
                     null;
@@ -263,26 +297,10 @@ function bistro(str) {
         }
     }
 
-    function mySubStr(str, index, length) {
-        if (length == undefined || length == null) {
-            length = str.length - index
-        }
-        var nexStr = ""
-        if (typeof str === 'string') {
-            for (var i = index; i <= index + length - 1; i++) {
-                nexStr += str[i]
-            }
-        } else {
-            console.log("Vos données ne sont pas de type string")
-            return;
-        }
-        return nexStr
-    }
-
     var result = priorityCalc(elem)
     result = round(result)
     return result
 }
 
-var res = bistro("(23-8)")
+var res = bistro("532-33")
 console.log(res)
