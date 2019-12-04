e7c04c4dc9a8b1a1a62740fbd50e909916ef6881
Théophile Noyer <theophile.noyer@hotmail.fr>
2019-12-04 15:35:30 +0100

    chat in progress

--- chat-socket/chat-node.js
+++ chat-socket/chat-node.js
@@ -3,26 +3,38 @@ const io = require('socket.io')(http);
 const port = 3000
 var ent = require('ent')
 var clients = []
+const chalk = require('chalk');
 
-io.on('connection', function (socket, pseudo) {
+io.on('connection', function (socket, nick, port) {
+    // socket.on('nouveau_port', function(port){
+    //     socket.port = port
+    //     // console.log('la?')
+    //     // http.listen(port, () => console.log(`server listening on port: ${port}`))
+    // })
     // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
-    socket.on('nouveau_client', function (pseudo) {
-        socket.pseudo = pseudo
-        console.log('user connected: ' + pseudo)
-        clients.push(socket.pseudo)
-        // console.log(clients)
-        socket.broadcast.emit('nouveau_client', pseudo)
+    socket.on('nouveau_client', function (nick) {
+        socket.nick = nick
+        console.log(chalk.green('user connected: ' + nick))
+        if(clients.includes(nick)){
+            console.log('Le pseudo ' + nick + ' est déjà utilisé')
+        } else {
+            clients.push(nick)
+            console.log(clients)
+            socket.broadcast.emit('nouveau_client', nick)
+        }
     });
-    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
+
+    // Dès qu'on reçoit un message, on récupère le nick de son auteur et on le transmet aux autres personnes
     socket.on('message', function (message) {
-        console.log(socket.pseudo + ' : ' + message)
-        socket.broadcast.emit('message', { pseudo: socket.pseudo, message: message })
+        console.log(chalk.blue(socket.nick + ' : ' + message))
+        socket.broadcast.emit('message', { nick: socket.nick, message: message })
     });
+    
     socket.on('disconnect', function () {
-        console.log(socket.pseudo + ' a quitter le salon')
-        socket.broadcast.emit('message', { pseudo: socket.pseudo, message: ' a quitté le salon' })
-        clients.splice(clients.indexOf(socket.pseudo), 1);
-        // console.log(clients)
+        console.log(chalk.red(socket.nick + ' a quitter le salon'))
+        socket.broadcast.emit('message', { nick: socket.nick, message: ' a quitté le salon' })
+        clients.splice(clients.indexOf(socket.nick), 1);
+        console.log(clients)
     });
 });
 
